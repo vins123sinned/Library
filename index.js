d2d75ec10632e23f25ceb6ef3b5a93cc2638e62d
Vinson <vinsonhe2@gmail.com>
2025-02-15 12:36:21 -0800

    Add function to display books
    Also added styling and structure to webpage so that the displayed
    content will look nice
    
    Also added

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ function addBookToLibrary(title, author, pageCount, readStatus) {
     myLibrary.push(new Book(title, author, pageCount, readStatus));
 }
 
+function displayBooks() {
+    const tableBody = document.querySelector('tbody');
+
+    for (const book of myLibrary) {
+        const tableRow = document.createElement('tr');
+        
+        for (const property in book) {
+            const tableData = document.createElement('td');
+            tableData.textContent = book[property];
+            
+            tableRow.appendChild(tableData);
+        }
+
+        tableBody.appendChild(tableRow);
+    }
+}
+
 addBookToLibrary('Harry Potter and the Sorcerer\'s Stone', 'J. K. Rowling', 223, true);
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
 addBookToLibrary('The Millionaire Fastlane', 'M. J. DeMarco', 334, true);
